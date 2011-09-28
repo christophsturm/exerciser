e0055a17e665c07d9d961b9643ec47e60bef2e22
Christoph Sturm <me@christophsturm.com>
2011-09-28 13:35:27 +0200

    make API simpler, support repeating requests

--- test/integration/exerciser_test.js
+++ test/integration/exerciser_test.js
@@ -1,8 +1,10 @@
 require.paths.push(__dirname + '/../../lib');
 
-nodeUnit = require("nodeunit");
-exerciser = require("exerciser");
-http = require("http");
+var nodeUnit = require("nodeunit"),
+    exerciser = require("exerciser"),
+    http = require("http"),
+    assert = require("assert");
+
 
 module.exports = nodeUnit.testCase({
 
@@ -14,20 +16,21 @@ module.exports = nodeUnit.testCase({
           res.end(req.url);
       });
       svr.listen(9999);
-
+      timeout = setTimeout(function () {assert.fail(null,null, "timeout"); },10000);
     callback();
   },
   "can run http requests": function(assert) {
       e = new exerciser.Exerciser({host:'127.0.0.1',port:9999});
-      e.paths = ['/blah','/blah'];
-      e.run(function() {
-            assert.equal(requests, 2, "should run 2 http request before calling callback");
+      e.run('/blah',10,function() {
+            assert.equal(requests, 10, "should run 10 http request before calling callback");
             assert.done();
           }
       )
   },
   tearDown: function(callback) {
+      clearTimeout(timeout);
       svr.close();
       callback();
+
   }
 });
